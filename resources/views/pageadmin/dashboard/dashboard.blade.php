2137cbdc11f5ae16b00be6ab1ec442cb19d8001b
botvi <younoob07mei@gmail.com>
2024-07-10 22:39:38 +0700

    main

--- resources/views/pageadmin/dashboard/dashboard.blade.php
+++ resources/views/pageadmin/dashboard/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="row">
-            <div class="col-lg-8 mb-4 order-0">
+            <div class="col-lg-12 mb-4 order-0">
                 <div class="card">
                     <div class="d-flex align-items-end row">
                         <div class="col-sm-7">
@@ -17,62 +17,11 @@
                         </div>
                         <div class="col-sm-5 text-center text-sm-left">
                             <div class="card-body pb-0 px-0 px-md-4">
-                                <img src="{{ asset('admin') }}/assets/img/illustrations/man-with-laptop-light.png"
-                                    height="140" alt="View Badge User"
-                                    data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                                    data-app-light-img="illustrations/man-with-laptop-light.png" />
+                                <img src="{{ asset('env') }}/udinus.png" class="mb-4" width="130px" srcset="">
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-md-4 order-1">
-                <div class="row">
-                    <div class="col-lg-6 col-md-12 col-6 mb-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="card-title d-flex align-items-start justify-content-between">
-                                    <div class="avatar flex-shrink-0">
-                                        <i class='bx bxs-notepad bg-success p-2 text-white rounded' ></i>
-                                    </div>
-                                    <div class="dropdown">
-                                        <button class="btn p-0" type="button" id="cardOpt3" data-bs-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false">
-                                            <i class="bx bx-dots-vertical-rounded"></i>
-                                        </button>
-                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                                            <a class="dropdown-item" href="/ujikompetensi">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <span class="fw-semibold d-block mb-1">Uji Kompetensi</span>
-                                <h3 class="card-title mb-2">55</h3>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-12 col-6 mb-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="card-title d-flex align-items-start justify-content-between">
-                                    <div class="avatar flex-shrink-0">
-                                        <i class='bx bxs-image bg-primary p-2 text-white rounded' ></i>
-                                    </div>
-                                    <div class="dropdown">
-                                        <button class="btn p-0" type="button" id="cardOpt6" data-bs-toggle="dropdown"
-                                            aria-haspopup="true" aria-expanded="false">
-                                            <i class="bx bx-dots-vertical-rounded"></i>
-                                        </button>
-                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
-                                            <a class="dropdown-item" href="/tebak-gambar">View More</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <span>Tebak Gambar</span>
-                                <h3 class="card-title text-nowrap mb-1">55</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>           
+            </div>       
     </div>
 @endsection
